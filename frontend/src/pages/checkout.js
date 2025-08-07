e4430842979f4679531854033495f97d89b4d08f
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-08-07 22:24:10 +0530

    Update checkout.js

--- frontend/src/pages/checkout.js
+++ frontend/src/pages/checkout.js
@@ -10,18 +10,7 @@ export const CartProvider = ({ children }) => {
   const addToCart = (product) => {
     setCart((prev) => {
       const exists = prev.find(item => item.id === product.id);
-      if (exists) {
-        return prev.map(item =>
-          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
-        );
-      }
-      return [...prev, { ...product, qty: 1 }];
-    });
-  };
-
-  return (
-    <CartContext.Provider value={{ cart, addToCart }}>
-      {children}
+  
     </CartContext.Provider>
   );
 };
